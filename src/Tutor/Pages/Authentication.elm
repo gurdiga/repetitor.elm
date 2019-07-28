a09440a6c613417f54c00b0d1e4504d80b318c09
Vlad GURDIGA <gurdiga@gmail.com>
2019-07-28 20:09:13 +0300

    Deploy

---
+++ src/Tutor/Pages/Authentication.elm
@@ -0,0 +1,61 @@
+module Main exposing (main)
+
+import Browser
+import Html exposing (..)
+import Html.Attributes exposing (..)
+import Html.Events exposing (..)
+import Http
+import Json.Decode exposing (Decoder, field, string)
+
+
+main =
+    Browser.document
+        { init = init
+        , update = update
+        , subscriptions = subscriptions
+        , view = view
+        }
+
+
+type alias Model =
+    {}
+
+
+init : () -> ( Model, Cmd Msg )
+init _ =
+    ( {}, Cmd.none )
+
+
+type Msg
+    = Msg
+
+
+update : Msg -> Model -> ( Model, Cmd Msg )
+update msg model =
+    ( model, Cmd.none )
+
+
+subscriptions : Model -> Sub Msg
+subscriptions model =
+    Sub.none
+
+
+type alias Document msg =
+    { title : String
+    , body : List (Html msg)
+    }
+
+
+view : Model -> Browser.Document Msg
+view model =
+    { title = "Autentificare repetitor"
+    , body = [ pageContents model ]
+    }
+
+
+pageContents : Model -> Html Msg
+pageContents model =
+    div []
+        [ h1 [] [ text "Autentificare repetitor" ]
+        , p [] [ text "TODO: Create form" ]
+        ]
