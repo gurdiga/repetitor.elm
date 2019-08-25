fa5aeb586cc6243fe3a1fc04152b7f0963ae2d47
Vlad GURDIGA <gurdiga@gmail.com>
2019-08-26 00:04:22 +0300

    Introduce type: Field a

--- src/Tutor/Pages/RegistrationPage/RegistrationForm.elm
+++ src/Tutor/Pages/RegistrationPage/RegistrationForm.elm
@@ -1,4 +1,4 @@
-module Tutor.Pages.RegistrationPage.RegistrationForm exposing (FieldValueSet, RegistrationForm, ValueSet, emptyForm, getFieldValue, updateEmail, updateForm, updateName, updatePhoneNumber)
+module Tutor.Pages.RegistrationPage.RegistrationForm exposing (FieldSet, RegistrationForm, ValueSet, emptyForm, getFieldValue, updateEmail, updateForm, updateName, updatePhoneNumber)
 
 import Domain.Utils.Email exposing (Email, emailToString, makeEmail)
 import Domain.Utils.FieldValue exposing (FieldValue(..), fieldFieldValueFromString, makeValidFieldValue)
@@ -7,14 +7,27 @@ import Domain.Utils.PhoneNumber exposing (PhoneNumber, makePhoneNumber, phoneNum
 
 
 type RegistrationForm
-    = IncompleteRegistrationForm FieldValueSet
+    = IncompleteRegistrationForm FieldSet
     | CompleteRegistrationForm ValueSet
 
 
-type alias FieldValueSet =
-    { fullName : FieldValue FullName
-    , phoneNumber : FieldValue PhoneNumber
-    , email : FieldValue Email
+type alias Field a =
+    { value : FieldValue a
+    , hasBeenBlurred : Bool
+    }
+
+
+emptyField : Field a
+emptyField =
+    { value = EmptyFieldValue
+    , hasBeenBlurred = False
+    }
+
+
+type alias FieldSet =
+    { fullName : Field FullName
+    , phoneNumber : Field PhoneNumber
+    , email : Field Email
     }
 
 
@@ -28,39 +41,48 @@ type alias ValueSet =
 emptyForm : RegistrationForm
 emptyForm =
     IncompleteRegistrationForm
-        { fullName = EmptyFieldValue
-        , phoneNumber = EmptyFieldValue
-        , email = EmptyFieldValue
+        { fullName = emptyField
+        , phoneNumber = emptyField
+        , email = emptyField
         }
 
 
 updateName : RegistrationForm -> String -> RegistrationForm
 updateName form string =
-    updateForm form (\fieldValues -> { fieldValues | fullName = fieldFieldValueFromString makeFullName string })
+    updateForm form
+        (\({ fullName } as fields) ->
+            { fields | fullName = { fullName | value = fieldFieldValueFromString makeFullName string } }
+        )
 
 
 updatePhoneNumber : RegistrationForm -> String -> RegistrationForm
 updatePhoneNumber form string =
-    updateForm form (\fieldValues -> { fieldValues | phoneNumber = fieldFieldValueFromString makePhoneNumber string })
+    updateForm form
+        (\({ phoneNumber } as fields) ->
+            { fields | phoneNumber = { phoneNumber | value = fieldFieldValueFromString makePhoneNumber string } }
+        )
 
 
 updateEmail : RegistrationForm -> String -> RegistrationForm
 updateEmail form string =
-    updateForm form (\fieldValues -> { fieldValues | email = fieldFieldValueFromString makeEmail string })
+    updateForm form
+        (\({ email } as fields) ->
+            { fields | email = { email | value = fieldFieldValueFromString makeEmail string } }
+        )
 
 
-getFieldValue : RegistrationForm -> (FieldValueSet -> FieldValue a) -> FieldValue a
+getFieldValue : RegistrationForm -> (FieldSet -> Field a) -> FieldValue a
 getFieldValue form accessFunction =
-    incompleteFormFields form |> accessFunction
+    incompleteFormFields form |> accessFunction |> .value
 
 
-updateForm : RegistrationForm -> (FieldValueSet -> FieldValueSet) -> RegistrationForm
+updateForm : RegistrationForm -> (FieldSet -> FieldSet) -> RegistrationForm
 updateForm form updateFunction =
     let
-        ({ fullName, phoneNumber, email } as fieldValues) =
+        ({ fullName, phoneNumber, email } as fields) =
             incompleteFormFields form |> updateFunction
     in
-    case ( fullName, phoneNumber, email ) of
+    case ( fullName.value, phoneNumber.value, email.value ) of
         ( ValidFieldValue _ validFirstName, ValidFieldValue _ validPhoneNumber, ValidFieldValue _ validEmail ) ->
             CompleteRegistrationForm
                 { fullName = validFirstName
@@ -69,17 +91,17 @@ updateForm form updateFunction =
                 }
 
         _ ->
-            IncompleteRegistrationForm fieldValues
+            IncompleteRegistrationForm fields
 
 
-incompleteFormFields : RegistrationForm -> FieldValueSet
+incompleteFormFields : RegistrationForm -> FieldSet
 incompleteFormFields form =
     case form of
         IncompleteRegistrationForm fields ->
             fields
 
         CompleteRegistrationForm { fullName, phoneNumber, email } ->
-            { fullName = makeValidFieldValue fullName fullNameToString
-            , phoneNumber = makeValidFieldValue phoneNumber phoneNumberToString
-            , email = makeValidFieldValue email emailToString
+            { fullName = { value = makeValidFieldValue fullName fullNameToString, hasBeenBlurred = True }
+            , phoneNumber = { value = makeValidFieldValue phoneNumber phoneNumberToString, hasBeenBlurred = True }
+            , email = { value = makeValidFieldValue email emailToString, hasBeenBlurred = True }
             }
